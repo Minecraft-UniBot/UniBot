b49a84a9c01dc80b9821b0ac984f1fb93d7bd0ea
LonelySail <xiaocaicai_email@sina.com>
2026-07-20 13:05:29 +0800

    更新：重构帮助。

--- Scripts/Render.py
+++ Scripts/Render.py
@@ -1,15 +1,4 @@
-from nonebot import require
-
-from .Config import config
-
-require('nonebot_plugin_htmlrender')
-from nonebot_plugin_htmlrender import template_to_pic  # noqa: E402
-
-template_path = './Resources/Images'
+from html2pic import Html2Pic
 
 
-async def render_template(template_name: str, size: tuple, **kwargs):
-    width, height = size
-    kwargs.setdefault('background', config.image_background)
-    page = {'viewport': {'width': width, 'height': height}, 'base_url': 'file://' + template_path}
-    return await template_to_pic(template_path, template_name, kwargs, pages=page, wait=1)
+
